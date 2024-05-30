5b30329ca528913ab471fe4778b642615ea656f7
goodzcyabc <903872978@qq.com>
2024-05-30 12:15:13 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,96 @@
+export default {
+  // page1
+  page1: {
+    titleEn: 'Hello,I`m Chenyu Zhang',
+    title: 'A Newcomer Running in Front-End Road', //Brief self-intro
+  },
+  // page2
+  page2: {
+    authorImg: 'page1.jpg', // profile picture
+    xinhui: 'I am Chenyu Zhang, a freshman in University of Waterloo',
+    qiuzhi: 'Front End Developer', // Job Seeking
+    guanyuwo: 'Learning front-end JavaScript HTML, CSS basic technology and Vue framework UI framework, Nodejs and so on, I hope to keep progressing together with everyone!' // About me
+  },
+  // page3
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: 'UI Design',
+    msg: ['PS cutting', 'interface layout']
+  },{
+    icon: 'icon-diannao',
+    title: 'Web Dev',
+    msg: ['Responsive Page', 'css、scss',]
+  }, {
+    icon: 'icon-qianbi1',
+    title: 'Front End Function',
+    msg: ['using JS to achieve common interactive functions', 'Reading backend data and rendering with AJAX']
+  },  {
+    icon: 'icon-shouji',
+    title: 'Frame',
+    msg: ['vue frame','UI frame、express frame、mysql',]
+  }],
+  // page4
+  page4: {
+    // experience
+    course: [{
+      date: '2024/9-Now',
+      desc: {
+        title: 'University of Waterloo, CA',
+        list: ['Making uniapps','Making Website with HTML,CSS,Javascript','Vue Related Projects']
+      }
+    }, {
+      date: '2021/9——2024/6',
+      desc: {
+        title: 'Nanjing Foreign Language School, CHN',
+        list: ['Learning C、C++、Python', 'Learning Full Stack Online', 'Creating Online Resume Pages']
+      }
+    }],
+    // my skills
+    singlelist: [{
+      title: 'bootsrap',
+      text: 'Learning'
+    }, {
+      title: 'Element-ui',
+      text: 'Learning'
+    }, {
+      title: 'mint-ui',
+      text: 'Learning'
+    }, {
+      title: 'scss',
+      text: 'Learning'
+    }],
+    // Skill
+    proresslist: [{
+      title: 'HTML、CSS、SCSS',
+      value: '90%'
+    }, {
+      title: 'JS、JQ',
+      value: '90%'
+    }, {
+      title: 'Vue',
+      value: '90%'
+    }, {
+      title: 'NodeJs',
+      value: '80%'
+    }, {
+      title: 'sql',
+      value: '80%'
+    }]
+  },
+  // page5
+  page5: [{
+    title: 'Online Resume',
+    content: 'Building a personal resume website using HTML, CSS, Javascript, etc.',
+    image: 'box1.png',
+    href: ''
+  }],
+  // page6
+  page6: {
+    github: 'https://github.com/goodzcyabc',
+    bili: 'https://space.bilibili.com/470985570?spm_id_from=333.337.0.0',
+    email: 'Mailto:903872978@qq.com',
+    zhihu: 'https://www.zhihu.com/people/zcy-51-51',
+    weixin: "<img style='width:120px' src='./src/img/weix.jpg' alt=''>",
+    qq: "<img style='width:120px' src='./src/img/qq.jpg' alt=''>"
+  }
+}
